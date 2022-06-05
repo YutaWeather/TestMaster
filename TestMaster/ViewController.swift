88df16fdc2ed04f8f3ae18ed8293f2bc3cc479d3
Yuta Fujii <yutayutakid21@gmail.com>
2022-06-05 12:25:44 +0900

    マスター

--- TestMaster/ViewController.swift
+++ TestMaster/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("マスター")
         // Do any additional setup after loading the view.
     }
 
